77213eb162524f47063225e91a7baae63356ddb2
Adam Collins <adam.w.collins@gmail.com>
2010-03-25 00:05:36 -0700

    Random integer stream generator

---
+++ generators/random.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+#
+# random.rb:
+#   generate random integer sequences
+#
+# usage:
+#   ./random.rb | head -n 500   # 500 numbers
+#
+
+min_value = (ARGV[0] || 1).to_i
+max_value = (ARGV[1] || 1000).to_i
+
+# ensure proper range order
+if (min_value > max_value)
+  (min_value, max_value) = [max_value, min_value]
+end
+
+range = max_value - min_value + 1
+srand() # make it truly randomish
+
+begin
+  loop do
+    puts rand(range) + min_value
+  end
+rescue
+  # nothing
+end
+
+
